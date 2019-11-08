4ea56ad7c855a20e2192840b2b9a84b8634a77ad
Amol <amolgadge663@gmail.com>
2019-11-08 18:08:42 +0530

    This is my first commit with my first ever android kotlin app OnClickListener On Button

---
+++ app/src/main/java/com/amolsoftwares/rollerdice/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.amolsoftwares.rollerdice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnRoll: Button =
+            findViewById(R.id.roll_btn) //This way we can get the tbn ref in kotlin
+        //btnRoll.text = "Lets Roll a dice" //This will change the btn txt
+        btnRoll.setOnClickListener {
+            Toast.makeText(this, "This btn is cliked", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+}
