5e8e3831e782d42fcf6cea5928c7411b77df6174
Amol <amolgadge663@gmail.com>
2019-11-08 18:31:09 +0530

    Toast massage

--- app/src/main/java/com/amolsoftwares/rollerdice/MainActivity.kt
+++ app/src/main/java/com/amolsoftwares/rollerdice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.amolsoftwares.rollerdice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -15,8 +16,16 @@ class MainActivity : AppCompatActivity() {
             findViewById(R.id.roll_btn) //This way we can get the tbn ref in kotlin
         //btnRoll.text = "Lets Roll a dice" //This will change the btn txt
         btnRoll.setOnClickListener {
-            Toast.makeText(this, "This btn is cliked", Toast.LENGTH_SHORT).show() //This is toast
+            //Toast.makeText(this, "This btn is cliked", Toast.LENGTH_SHORT).show() //This is toast
+
+            rollDice() //This is kotlin function or method where declared
 
         }
+    }
+
+    private fun rollDice() {
+        val txtResult: TextView = findViewById(R.id.result_txt)
+        txtResult.text = "Dice Rolled!" //This will change the text of TextView when btn is clicked.
+
     }
 }
